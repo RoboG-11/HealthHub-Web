9c398c50eac9b2d81f817e595f89448c7314d0b0
Bian Rivera Martinez <brian.rivera@cua.uam.mx>
2024-03-01 22:55:54 -0600

    modification of Swagger documentation

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -95,7 +95,9 @@ return [
             /*
              * Edit to set path where swagger ui assets should be stored
             */
-            'swagger_ui_assets_path' => env('L5_SWAGGER_UI_ASSETS_PATH', 'vendor/swagger-api/swagger-ui/dist/'),
+            // 'swagger_ui_assets_path' => env('L5_SWAGGER_UI_ASSETS_PATH', 'vendor/swagger-api/swagger-ui/dist/'),
+            'swagger_ui_assets_path' => 'https://healthhub-i0f3.onrender.com/vendor/swagger-api/swagger-ui/dist/',
+
 
             /*
              * Absolute path to directories that should be excluded from scanning
